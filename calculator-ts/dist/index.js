77740bd3a87629be9c433e6f5dcd7b336c5c8183
qurat150 <qurat.1432@gmail.com>
2022-10-18 00:02:06 +0500

    calculator-ts

--- calculator-ts/dist/index.js
+++ calculator-ts/dist/index.js
@@ -1,8 +1,25 @@
 "use strict";
+let str = "";
+let input = document.querySelector("#screen");
 document.querySelectorAll(".button").forEach((button) => {
     button.addEventListener("click", (e) => {
-        console.log(e.target.innerHTML);
-        let gettingInnerText = e.target.innerHTML;
-        let input = document.querySelector("#screen").innerHTML = gettingInnerText;
+        let btnInnerText = e.target.innerHTML;
+        if (btnInnerText === "AC") {
+            str = input.value = "";
+        }
+        else if (btnInnerText === "=") {
+            try {
+                input.value = window.eval(str);
+            }
+            catch (e) {
+                input.value = "Error";
+            }
+        }
+        else {
+            if (input.value == "Error") {
+                input.value = "";
+            }
+            str = input.value += btnInnerText;
+        }
     });
 });
--- calculator-ts/src/index.ts
+++ calculator-ts/src/index.ts
@@ -1,8 +1,22 @@
-// let str:string = "";
-document.querySelectorAll(".button").forEach((button)=>{
-    button.addEventListener("click" , (e)=>{
-        console.log((<HTMLButtonElement>e.target).innerHTML);
-       let  gettingInnerText = (<HTMLButtonElement>e.target).innerHTML;
-        let input = (<HTMLInputElement>document.querySelector("#screen")).innerHTML =gettingInnerText 
-    })
-})
+let str: string = "";
+let input = <HTMLInputElement>document.querySelector("#screen");
+document.querySelectorAll(".button").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let btnInnerText: string = (<HTMLButtonElement>e.target).innerHTML;
+    if (btnInnerText === "AC") {
+      str = input.value = "";
+    } else if (btnInnerText === "=") {
+      try {
+        input.value = (window as any).eval(str);
+      } catch (e: any) {
+        input.value = "Error";
+      }
+    } else {
+      if (input.value == "Error") {
+        input.value = "";
+      }
+
+      str = input.value += btnInnerText;
+    }
+  });
+});
