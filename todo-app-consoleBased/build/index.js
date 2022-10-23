2ce2a206ea210180933ed726535ce1f0a3403e96
qurat150 <qurat.1432@gmail.com>
2022-10-23 10:49:09 +0500

    some changes

--- todo-app-consoleBased/build/index.js
+++ todo-app-consoleBased/build/index.js
@@ -1,19 +1,55 @@
 "use strict";
 const inquirer = require("inquirer");
-inquirer
-    .prompt([{
-        type: "input",
-        name: "todos",
-        message: "Add Todo here."
-    }]).then((result) => {
-    const todoHandler = () => {
-        let todoList = [];
-        todoList.push(result.todos);
-        todoList.forEach((todos) => {
-            console.log(todos);
-        });
-    };
-    for (var i = 0; i < 5; i++) {
+let todoList = [];
+var i = 1;
+const todoMain = () => {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "todos",
+            message: "Add Todo here."
+        }]).then((result) => {
+        const todoHandler = () => {
+            console.log(i);
+            todoList.push(result.todos);
+            i++;
+            if (i <= 5) {
+                todoMain();
+            }
+            else {
+                todoList.forEach((addingTodo) => {
+                    console.log(addingTodo);
+                });
+                inquirer
+                    .prompt([{
+                        type: "list",
+                        name: "deletionOfArrays",
+                        message: "Want to delete a Todo?",
+                        choices: ["Yes", "No"]
+                    }]).then((result) => {
+                    if (result.deletionOfArrays == "Yes") {
+                        inquirer
+                            .prompt([{
+                                type: "input",
+                                name: "deleteArrays",
+                                message: "Which todo do you want to Delete?",
+                            }]).then((result) => {
+                            console.log("hello");
+                            todoList.filter((checkingExistance) => {
+                                let existanceOfTodo = result.deleteArrays == checkingExistance;
+                                if (existanceOfTodo == true) {
+                                    let index = result.deleteArrays;
+                                    console.log(todoList.indexOf(index));
+                                    todoList.splice(index, 1);
+                                    console.log(todoList);
+                                }
+                            });
+                        });
+                    }
+                });
+            }
+        };
         todoHandler();
-    }
-});
+    });
+};
+todoMain();
--- todo-app-consoleBased/src/index.ts
+++ todo-app-consoleBased/src/index.ts
@@ -1,18 +1,56 @@
 const inquirer = require("inquirer")
 
-inquirer
-    .prompt([{
-        type: "input",
-        name: "todos",
-        message: "Add Todo here."
-    }]).then((result: any) => {
-        const todoHandler = () => {
-            let todoList: string[] = [];
-            todoList.push(result.todos)
-            todoList.forEach((todos) => {
-                console.log(todos);
-            })
-
-        }
-    })
-
+let todoList: string[] = [];
+var i = 1;
+const todoMain = () => {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "todos",
+            message: "Add Todo here."
+        }]).then((result: any) => {
+            const todoHandler = () => {
+                console.log(i);
+                todoList.push(result.todos)
+                i++;
+                if (i <= 5) {
+                    todoMain()
+                }
+                else {
+                    todoList.forEach((addingTodo) => {
+                        console.log(addingTodo);
+                    })
+                    inquirer
+                        .prompt([{
+                            type: "list",
+                            name: "deletionOfArrays",
+                            message: "Want to delete a Todo?",
+                            choices: ["Yes", "No"]
+                        }]).then((result: any) => {
+                            if (result.deletionOfArrays == "Yes") {
+                                inquirer
+                                    .prompt([{
+                                        type: "input",
+                                        name: "deleteArrays",
+                                        message: "Which todo do you want to Delete?",
+                                    }]).then((result: any) => {
+                                        console.log("hello");
+                                        todoList.filter((checkingExistance) => {
+                                            let existanceOfTodo =  result.deleteArrays == checkingExistance;
+                                            if(existanceOfTodo == true){
+                                                let index = result.deleteArrays
+                                                console.log(todoList.indexOf(index));
+                                                todoList.splice(index , 1)
+                                                console.log(todoList);
+                                                
+                                            }
+                                        })
+                                    })
+                            }
+                        })
+                }
+            }
+            todoHandler()
+        })
+}
+todoMain()
